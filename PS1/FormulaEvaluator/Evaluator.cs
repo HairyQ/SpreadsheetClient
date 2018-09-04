14a3ff97b2526dadccc8d949f30118ba8d7b8f77
hquick4 <hquick4@gmail.com>
2018-09-03 18:34:24 -0600

    Added VariableIntegerInstructions helper function for integer & variable tokens

--- PS1/FormulaEvaluator/Evaluator.cs
+++ PS1/FormulaEvaluator/Evaluator.cs
@@ -14,7 +14,7 @@ namespace FormulaEvaluator
 
         private static Stack<string> operators;
 
-        private static Stack<string> values;
+        private static Stack<int> values;
 
         private static int currVal;
 
@@ -24,17 +24,62 @@ namespace FormulaEvaluator
 
             foreach (string substring in substrings)
             {
+                //Regex for finding just integers
+                if (Regex.IsMatch(substring, @"(\s?[0-9]{1,}\s?)"))
+                {
+                    currVal = int.Parse(substring);
+                    VariableIntegerInstructions(currVal);
+                }
                 //Regex for finding variables
-                if (Regex.IsMatch(substring, @"\s?[A-Za-z]{1,}[0-9]{1,}\s?"))
+                else if (Regex.IsMatch(substring, @"(\s?[A-Za-z]{1,}[0-9]{1,}\s?)"))
                 {
                     currVal = variableEvaluator(substring);
+                    VariableIntegerInstructions(currVal);
                 }
+                //Regex for finding '+' and '-' operators
+                else if (Regex.IsMatch(substring, @"(\s?\+|\-\s?)"))
+                {
 
+                }
+                //Regex for finding '(' left parentheses
+                else if (Regex.IsMatch(substring, @"(\s?\(\s?)"))
+                {
 
+                }
+                //Regex for finding ')' right parentheses
+                else if (Regex.IsMatch(substring, @"(\s?\)\s?)"))
+                {
 
+                }
+                
             }
 
             return 0;
+        }
+
+        /// <summary>
+        /// Integer and Variable tokens use the same instructions, so to avoid redundant code, I included this function.
+        /// 
+        /// </summary>
+        /// <param name="currVal">Integer or Variable Value</param>
+        private static void VariableIntegerInstructions(int currVal)
+        {
+            if (operators.Peek().Equals("*"))
+            {
+                operators.Pop();
+                currVal *= values.Pop();
+                values.Push(currVal);
+            }
+            else if (operators.Peek().Equals("/"))
+            {
+                operators.Pop();
+                currVal /= values.Pop();
+                values.Push(currVal);
+            }
+            else
+            {
+                values.Push(currVal);
+            }
         }
 
     }
