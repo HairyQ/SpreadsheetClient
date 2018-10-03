7daae6aade1597f8a9f1dd166a8dc268ded98b59
Harrison Quick <hquick4@gmail.com>
2018-10-02 18:25:27 -0600

    Added comments, improved general readability

--- PS4/Spreadsheet/Spreadsheet.cs
+++ PS4/Spreadsheet/Spreadsheet.cs
@@ -12,8 +12,6 @@ namespace SS
 
     {
         /// <summary>
-        /// This will be modified to fit specifications for PS5
-        /// 
         /// Cell class for setting and getting cell's contents which could be of type Formula, String or Double,
         /// as well as determining Cell's name
         /// </summary>
@@ -23,28 +21,29 @@ namespace SS
             private Object contents;
             private Object value;
 
-            public Cell(String newName)
-            {
-                name = newName;
-                contents = "";
-            }
+            public Cell(String newName) { name = newName; contents = ""; } //Cell constructor
 
-            public Object GetContents()
-            {
-                return contents;
-            }
+            public Object GetContents() { return contents; }
 
-            public void SetContents(object o)
-            {
-                contents = o;
-            }
+            public void SetContents(object o) { contents = o; }
         }
 
+        /// <summary>
+        /// Dictionary mapping cell names to cell objects.
+        /// Contains all cells whose values have been 'changed', thus a list of all initialized cells
+        /// </summary>
         private Dictionary<string, Cell> allCells;
 
+        /// <summary>
+        /// Dependency graph representing dependencies between cells according to their formulae
+        /// </summary>
         private DependencyGraph dependencies;
 
-        public override bool Changed { get => throw new NotImplementedException(); protected set => throw new NotImplementedException(); }
+        /// <summary>
+        /// Field determining whether or not this version of this Spreadsheet has been 'changed'
+        /// </summary>
+        public override bool Changed { get => throw new NotImplementedException();
+            protected set => throw new NotImplementedException(); }
 
         /// <summary>
         /// Zero-argument public constructor
@@ -100,9 +99,10 @@ namespace SS
         /// <returns>Contents of a cell, whether the contents be of type String, Double, or Formula</returns>
         public override object GetCellContents(string name)
         {
+            name = Normalize(name); //Normalize name according to user's normalizer
             CheckIfNullOrInvalidVariableName(name);
 
-            if (!allCells.ContainsKey(name))
+            if (!allCells.ContainsKey(name)) //Empty cell implies 'empty string' contents
                 return "";
 
             return allCells[name].GetContents();
@@ -129,9 +129,7 @@ namespace SS
         /// <param name="number">Double Value of Cell</param>
         /// <returns>HashSet containing cell's name and the names of all dependent cells to that cell</returns>
         protected override ISet<string> SetCellContents(string name, double number)
-        {
-            return SetCellHelper(name, number);
-        }
+        { return SetCellHelper(name, number); }
 
         /// <summary>
         /// Sets the contents of a cell, given that cell's name, to that cell's value in the
@@ -168,12 +166,19 @@ namespace SS
             return SetCellHelper(name, formula);
         }
 
+        /// <summary>
+        /// Finds the dependents of a given cell that are considered "direct" dependents (the variables 
+        /// directly referenced by the formula in this cell)
+        /// </summary>
+        /// <param name="name">Name of cell</param>
+        /// <returns>An Enumeration of the direct dependents of cell's formula</returns>
         protected override IEnumerable<string> GetDirectDependents(string name)
         {
             if (name == null)
                 throw new ArgumentNullException();
 
-            CheckIfNullOrInvalidVariableName(name);
+            name = Normalize(name); //Normalize name of cell according to user
+            CheckIfNullOrInvalidVariableName(name); //Check validity of cell name
 
             if (GetCellContents(name).GetType() == typeof(Formula))
             {
@@ -182,6 +187,50 @@ namespace SS
                     yield return s;
                 }
             }
+        }
+
+        public override string GetSavedVersion(string filename)
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        public override void Save(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object GetCellValue(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Public accessor method for setting the contents of any cell to a Formula, Double, or String.
+        /// First, normalizes and checks the validity of name, then parses content into whatever type
+        /// is implied by its string representation (Formula, Double, or String), then passes name and
+        /// content to the appropriate setCellContents() helper method (according to content's type), 
+        /// and returns the resulting Set of cells that depend, directly or indirectly, on (name's) cell
+        /// </summary>
+        /// <param name="name">Name of given cell</param>
+        /// <param name="content">Cell's new contents</param>
+        /// <returns></returns>
+        public override ISet<string> SetContentsOfCell(string name, string content)
+        {
+            Changed = true; //Spreadsheet has now officially been changed
+            double newDouble;   //Initialized double in case content is of type double
+
+            name = Normalize(name); //Normalize name according to user's normalize delegate or default Function
+            CheckIfNullOrInvalidVariableName(name);
+
+            if (content.ToCharArray()[0] == '=')                    //Content is Formula
+                return SetCellContents(name, new Formula(name.Substring(1), Normalize, IsValid));
+
+            else if (Double.TryParse(content, out newDouble))       //Content is Double
+                return SetCellContents(name, newDouble);
+
+            else                                                    //Content is String
+                return SetCellContents(name, content);
         }
 
         /// <summary>
@@ -205,7 +254,7 @@ namespace SS
                 {
                     return new HashSet<string>() { name };
                 }
- 
+
                 Cell newCell = new Cell(name);
                 newCell.SetContents(contents);
                 allCells.Add(name, newCell);
@@ -232,43 +281,11 @@ namespace SS
                 throw new InvalidNameException();
 
             //Regex for finding valid variable names
-            if (!Regex.IsMatch(name, @"^[A-Za-z_]{1,}[A-Za-z_|\d]*$"))
+            if (!Regex.IsMatch(name, @"^[A-Za-z]+[\d]+$"))
                 throw new InvalidNameException();
-        }
 
-        public override string GetSavedVersion(string filename)
-        {
-            
-            throw new NotImplementedException();
-        }
-
-        public override void Save(string filename)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override object GetCellValue(string name)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override ISet<string> SetContentsOfCell(string name, string content)
-        {
-            Changed = true; //Spreadsheet has now officially been changed
-            double newDouble;   //Initialized double in case content is of type double
-
-            if (content.ToCharArray()[0] == '=')
-            {
-                return SetCellContents(name, new Formula(name.Substring(1)));
-            }
-            else if (Double.TryParse(content, out newDouble))
-            {
-                return SetCellContents(name, newDouble);
-            }
-            else
-            {
-                return SetCellContents(name, content);
-            }
+            if (!IsValid(name)) //name is invalid according to user
+                throw new InvalidNameException();
         }
     }
 }
