cb2588b3f76a6d7bed6933d01d58e0985cf31b5d
Harrison Quick <hquick4@gmail.com>
2018-10-16 17:15:08 -0600

    Fixed build errors

--- PS6/WindowsFormsApp1/Form1.Designer.cs
+++ PS6/WindowsFormsApp1/Form1.Designer.cs
@@ -29,47 +29,23 @@
         private void InitializeComponent()
         {
             this.spreadsheetPanel1 = new SS.SpreadsheetPanel();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // spreadsheetPanel1
             // 
-            this.spreadsheetPanel1.Location = new System.Drawing.Point(3, 27);
+            this.spreadsheetPanel1.Location = new System.Drawing.Point(2, 8);
             this.spreadsheetPanel1.Name = "spreadsheetPanel1";
             this.spreadsheetPanel1.Size = new System.Drawing.Size(797, 431);
             this.spreadsheetPanel1.TabIndex = 0;
-            // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.Controls.Add(this.menuStrip1);
-            this.flowLayoutPanel1.Controls.Add(this.spreadsheetPanel1);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(783, 432);
-            this.flowLayoutPanel1.TabIndex = 1;
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(202, 24);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.flowLayoutPanel1);
-            this.MainMenuStrip = this.menuStrip1;
+            this.Controls.Add(this.spreadsheetPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.flowLayoutPanel1.ResumeLayout(false);
-            this.flowLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -77,8 +53,6 @@
         #endregion
 
         private SS.SpreadsheetPanel spreadsheetPanel1;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private System.Windows.Forms.MenuStrip menuStrip1;
     }
 }
 
