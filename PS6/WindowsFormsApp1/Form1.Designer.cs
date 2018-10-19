42385253a0212234e4384fcffc90b8807b52cdda
lrfalslev <37004519+lrfalslev@users.noreply.github.com>
2018-10-18 20:26:26 -0600

    flesh out gui, link spreadsheet to spreadsheetPanel

--- PS6/WindowsFormsApp1/Form1.Designer.cs
+++ PS6/WindowsFormsApp1/Form1.Designer.cs
@@ -29,30 +29,70 @@
         private void InitializeComponent()
         {
             this.spreadsheetPanel1 = new SS.SpreadsheetPanel();
+            this.cellNameField = new System.Windows.Forms.TextBox();
+            this.cellContentsField = new System.Windows.Forms.TextBox();
+            this.setButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // spreadsheetPanel1
             // 
-            this.spreadsheetPanel1.Location = new System.Drawing.Point(2, 8);
+            this.spreadsheetPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.spreadsheetPanel1.Location = new System.Drawing.Point(12, 53);
             this.spreadsheetPanel1.Name = "spreadsheetPanel1";
-            this.spreadsheetPanel1.Size = new System.Drawing.Size(797, 431);
+            this.spreadsheetPanel1.Size = new System.Drawing.Size(797, 403);
             this.spreadsheetPanel1.TabIndex = 0;
+            // 
+            // cellNameField
+            // 
+            this.cellNameField.Location = new System.Drawing.Point(28, 8);
+            this.cellNameField.Name = "cellNameField";
+            this.cellNameField.ReadOnly = true;
+            this.cellNameField.Size = new System.Drawing.Size(146, 22);
+            this.cellNameField.TabIndex = 1;
+            this.cellNameField.TabStop = false;
+            this.cellNameField.Text = "A1";
+            // 
+            // cellContentsField
+            // 
+            this.cellContentsField.Location = new System.Drawing.Point(180, 8);
+            this.cellContentsField.Name = "cellContentsField";
+            this.cellContentsField.Size = new System.Drawing.Size(146, 22);
+            this.cellContentsField.TabIndex = 3;
+            // 
+            // setButton
+            // 
+            this.setButton.Location = new System.Drawing.Point(332, 7);
+            this.setButton.Name = "setButton";
+            this.setButton.Size = new System.Drawing.Size(75, 23);
+            this.setButton.TabIndex = 4;
+            this.setButton.Text = "Set Cell";
+            this.setButton.UseVisualStyleBackColor = true;
+            this.setButton.Click += new System.EventHandler(this.setButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.setButton);
+            this.Controls.Add(this.cellContentsField);
+            this.Controls.Add(this.cellNameField);
             this.Controls.Add(this.spreadsheetPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private SS.SpreadsheetPanel spreadsheetPanel1;
+        private System.Windows.Forms.TextBox cellNameField;
+        private System.Windows.Forms.TextBox cellContentsField;
+        private System.Windows.Forms.Button setButton;
     }
 }
 
--- PS6/WindowsFormsApp1/Form1.cs
+++ PS6/WindowsFormsApp1/Form1.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using SS;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,82 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        Spreadsheet sheet;
+
         public Form1()
         {
             InitializeComponent();
+
+            //Register displaySelection as listener to selectionChanged
+            spreadsheetPanel1.SelectionChanged += displaySelection;
+            spreadsheetPanel1.SetSelection(0, 0);
+
+            //enter acts as set button
+            this.AcceptButton = setButton;
+
+            sheet = new Spreadsheet(s => Regex.IsMatch(s, "^[A-Z]{1}[1-99]{1}$"), s => s.ToUpper(), "ps6");
+        }
+
+        
+        /// <summary>
+        /// Every time the selection changes, this method is called with the
+        /// Spreadsheet as its parameter. CellName and CellContents fields
+        /// are updated accordingly.
+        /// </summary>
+        /// <param name="ss"></param>
+        private void displaySelection(SpreadsheetPanel ss)
+        {
+            cellContentsField.Focus();
+            
+            ss.GetSelection(out int col, out int row);
+
+            string name = GetCellName(col, row);
+
+            cellNameField.Text = name;
+
+            //set CellContents from spreadsheet
+            cellContentsField.Text = sheet.GetCellContents(name).ToString();
+            
+            //highlight current contents
+            cellContentsField.SelectionStart = 0;
+            cellContentsField.SelectionLength = cellContentsField.Text.Length;
+        }
+
+        /// <summary>
+        /// Converts col and row ints from grid to 
+        /// corresponding spreadsheet cell name
+        /// </summary>
+        /// <param name="col"></param>
+        /// <param name="row"></param>
+        /// <returns></returns>
+        private string GetCellName(int col, int row)
+        {   
+            string name = "";
+
+            //convert column index to ASCII char
+            //65 offset so 0=A and 26=Z
+            char c = (char)(col + 65);
+
+            //Name is char from column followed by int from row
+            name += c;
+            name += row + 1;
+
+            return name;
+        }
+        
+
+        private void setButton_Click(object sender, EventArgs e)
+        {
+            spreadsheetPanel1.GetSelection(out int col, out int row);
+
+            string name = GetCellName(col, row);
+            string contents = cellContentsField.Text;
+
+            sheet.SetContentsOfCell(name, contents);
+
+            string value = sheet.GetCellValue(name).ToString();
+
+            spreadsheetPanel1.SetValue(col, row, value);
         }
     }
 }
