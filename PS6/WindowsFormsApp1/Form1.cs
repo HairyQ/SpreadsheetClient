c93795af4925cb6decf451ebff39040353d674d7
lrfalslev <37004519+lrfalslev@users.noreply.github.com>
2018-10-21 17:52:01 -0600

    fix IsValid regex, extract setCell method, add arrowkey listeners

--- PS6/WindowsFormsApp1/Form1.cs
+++ PS6/WindowsFormsApp1/Form1.cs
@@ -35,10 +35,13 @@ namespace WindowsFormsApp1
             this.AcceptButton = setButton;
 
             //create backing structure
-            sheet = new SS.Spreadsheet(s => Regex.IsMatch(s, "^[A-Z]{1}[1-9]{2}$"), s => s.ToUpper(), "ps6");
+            sheet = new SS.Spreadsheet(s => Regex.IsMatch(s, "^[A-Z]{1}[1-9]{1}[0-9]?$"), s => s.ToUpper(), "ps6");
         }
 
-
+        private void selectionChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("selection changed");
+        }
         /// <summary>
         /// Every time the selection changes, this method is called with the
         /// Spreadsheet as its parameter. CellName, CellValue, CellContents fields
@@ -54,7 +57,7 @@ namespace WindowsFormsApp1
             string name = GetCellName(col, row);
 
             cellNameField.Text = name;
-
+            
             //set CellContentsField from spreadsheet
             if (sheet.GetCellContents(name) is Formula)
                 cellContentsField.Text = "=" + sheet.GetCellContents(name).ToString();
@@ -94,34 +97,53 @@ namespace WindowsFormsApp1
 
         private void setButton_Click(object sender, EventArgs e)
         {
+            //get current cell
             spreadsheetPanel1.GetSelection(out int col, out int row);
 
-            string name = GetCellName(col, row);
+            //extract contents contents
             string contents = cellContentsField.Text;
+
+            SetCell(col, row, contents);
+        }
+
+        /// <summary>
+        /// takes string contents and column and row number,
+        /// converts col and row to a cell value, adds cell
+        /// to backing sheet and update gui accordingly
+        /// </summary>
+        /// <param name="col"></param>
+        /// <param name="row"></param>
+        /// <param name="contents"></param>
+        private void SetCell(int col, int row,  string contents)
+        {
+            string name = GetCellName(col, row);
             string value = "";
 
             try
             {
+                //store contents in backing sheet
                 sheet.SetContentsOfCell(name, contents);
 
+                //get value of cell from backing sheet
                 object valueObj = sheet.GetCellValue(name);
 
+                //handle formula errors
                 if (valueObj is Type FormulaError)
                 {
                     FormulaError fe = (FormulaError)valueObj;
                     value = fe.Reason;
                 }
+
+                //store value of cell
                 else
                     value = valueObj.ToString();
             }
-            catch (Exception ffe)
+            catch (FormulaFormatException ffe)
             {
                 //create error message window
                 MessageBox.Show(ffe.Message, "Formula Format Error");
                 string s = ffe.Message;
             }
-
-            //string value = sheet.GetCellValue(name).ToString();
 
             //update gui representation
             cellValueField.Text = value;
@@ -131,6 +153,38 @@ namespace WindowsFormsApp1
         private void openFileMenu(object sender, EventArgs e)
         {
             
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            //capture up arrow key
+            if (keyData == Keys.Up)
+            {
+                spreadsheetPanel1.GetSelection(out int col, out int row);
+                row++;
+                string name = GetCellName(col, row);
+                MessageBox.Show("You pressed Up arrow key");
+                return true;
+            }
+            //capture down arrow key
+            if (keyData == Keys.Down)
+            {
+                MessageBox.Show("You pressed Down arrow key");
+                return true;
+            }
+            //capture left arrow key
+            if (keyData == Keys.Left)
+            {
+                MessageBox.Show("You pressed Left arrow key");
+                return true;
+            }
+            //capture right arrow key
+            if (keyData == Keys.Right)
+            {
+                MessageBox.Show("You pressed Right arrow key");
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
         }
     }
 }
