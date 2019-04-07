cb79862691fa1d6507e97c083bfe4f70e2b34d4e
Harrison Quick <hquick4@gmail.com>
2019-04-07 17:21:25 -0600

    Need to clone

--- ConsoleApp2/ConsoleApp1/FullyAssociativeProgram.cs
+++ ConsoleApp2/ConsoleApp1/FullyAssociativeProgram.cs
@@ -12,9 +12,13 @@ namespace ConsoleApp2
         static int[] addresses = new int[] { 16, 20, 24, 28, 32, 36, 60, 64, 56, 60, 64, 68, 56, 60, 64, 72, 76, 92, 96, 100, 104, 108, 112, 120, 124, 128, 144, 148 };
         static int numRows;
         static int blockSize;
+        static int totalCPI;
+        static int numMisses;
 
         static void Main(string[] args)
         {
+            totalCPI = 0;
+            numMisses = 0;
             Console.WriteLine("How many rows?");
             Int32.TryParse(Console.ReadLine(), out numRows);
             Console.WriteLine("What is the block size (in bytes)?");
@@ -24,6 +28,15 @@ namespace ConsoleApp2
 
             c.firstIteration();
             c.addAll();
+
+            double averageCPI = (totalCPI * 1.0) / blockSize;
+            int numLRUandTAGBits = Convert.ToInt32(Math.Ceiling(Math.Log(numRows * 1.0, 2.0)));
+            int numOffsetBits = Convert.ToInt32(Math.Ceiling(Math.Log(blockSize * 1.0, 2.0)));
+            int totalBits = numRows * ((blockSize * 8) + 1 + (numLRUandTAGBits * 2) + numOffsetBits);
+            double missRate = numMisses * 1.0 / addresses.Length;
+
+            Console.WriteLine("Average CPI: " + averageCPI + "\nTotal CPI: " + totalCPI + "\nTotal Bits: " + totalBits
+                + "\nMiss Rate: " + missRate);
 
             Console.Read();
         }
@@ -56,36 +69,47 @@ namespace ConsoleApp2
 
                 if (!totalBlocks.ContainsKey(tag))
                 {
+                    foreach (block b in totalBlocks.Values)
+                        b.LRU--;
+
                     totalBlocks.Add(tag, new block(tag));
-                    Console.WriteLine("Accessing: " + address + " (tag: " + tag + "): miss - cached to row ");
+                    Console.WriteLine("Accessing: " + address + " (tag: " + tag + "): miss");
+                    numMisses++;
+                    totalCPI += 12 + blockSize;
                 }
                 else if (!totalBlocks[tag].data[offset].Equals(address))
                 {
                     totalBlocks[tag].data[offset] = address;
+
+                    foreach (block b in totalBlocks.Values)
+                        b.LRU--;
+
                     totalBlocks[tag].LRU = numRows;
-                    foreach(int x in addresses)
-                        
-                    Console.WriteLine("Accessing: " + address + " (tag: " + tag + "): miss - cached to row ");
+
+
+                    Console.WriteLine("Accessing: " + address + " (tag: " + tag + "): miss");
+                    totalCPI += 12 + blockSize;
+                    numMisses++;
                 }
                 else
                 {
                     Console.WriteLine("Accessing: " + address + " (tag: " + tag + "): hit");
+                    totalCPI++;
                 }
             }
 
             public void AddDataWithoutPrinting(int address)
             {
-                int row = (address / blockSize) % numRows;
                 int tag = address / (blockSize * numRows);
                 int offset = address % blockSize;
 
-                if (!totalBlocks.ContainsKey(row))
+                if (!totalBlocks.ContainsKey(tag))
                 {
-                    totalBlocks.Add(row, new block(tag));
+                    totalBlocks.Add(tag, new block(tag));
                 }
-                else if (!totalBlocks[row].data[offset].Equals(address))
+                else if (!totalBlocks[tag].data[offset].Equals(address))
                 {
-                    totalBlocks[row].data[offset] = address;
+                    totalBlocks[tag].data[offset] = address;
                 }
             }
         }
--- ConsoleApp2/ConsoleApp2/DirectMappedProgram.cs
+++ ConsoleApp2/ConsoleApp2/DirectMappedProgram.cs
@@ -7,11 +7,10 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
-    class Program
+    class DirectMappedProgram
     {
         static int[] addresses = new int[] { 16, 20, 24, 28, 32, 36, 60, 64, 56, 60, 64, 68, 56, 60, 64, 72, 76, 92, 96, 100, 104, 108, 112, 120, 124, 128, 144, 148 };
-        static int numRows;
-        static int blockSize;
+        static int numRows, blockSize, totalCPI, numMisses;
 
         static void Main(string[] args)
         {
@@ -19,11 +18,22 @@ namespace ConsoleApp2
             Int32.TryParse(Console.ReadLine(), out numRows);
             Console.WriteLine("What is the block size (in bytes)?");
             Int32.TryParse(Console.ReadLine(), out blockSize);
+            totalCPI = 0;
 
             Cache c = new Cache();
 
             c.firstIteration();
             c.addAll();
+
+            double averageCPI = (totalCPI * 1.0) / addresses.Length;
+            int numLRUandTAGBits = Convert.ToInt32(Math.Ceiling(Math.Log(numRows * 1.0, 2.0)));
+            int numOffsetBits = Convert.ToInt32(Math.Ceiling(Math.Log(blockSize * 1.0, 2.0)));
+            int numRowBits = Convert.ToInt32(Math.Ceiling(Math.Log(numRows * 1.0, 2.0)));
+            int totalBits = numRows * ((blockSize * 8) + 1 + numLRUandTAGBits + numOffsetBits + numRowBits);
+            double missRate = numMisses * 1.0 / addresses.Length;
+
+            Console.WriteLine("Average CPI: " + averageCPI + "\nTotal CPI: " + totalCPI + "\nTotal Bits: " + totalBits
+                + "\nMiss Rate: " + missRate);
 
             Console.Read();
         }
@@ -51,22 +61,27 @@ namespace ConsoleApp2
 
             public void AddData(int address)
             {
-                int row = (address/blockSize) % numRows;
+                int row = (address / blockSize) % numRows;
                 int tag = address / (blockSize * numRows);
                 int offset = address % blockSize;
 
                 if (!totalBlocks.ContainsKey(row))
                 {
                     totalBlocks.Add(row, new block(tag, row));
+                    totalCPI += 12 + blockSize;
+                    numMisses++;
                     Console.WriteLine("Accessing: " + address + " (tag: " + tag + "): miss - cached to row " + row);
                 }
                 else if (!totalBlocks[row].data[offset].Equals(address))
                 {
                     totalBlocks[row].data[offset] = address;
+                    totalCPI += 12 + blockSize;
+                    numMisses++;
                     Console.WriteLine("Accessing: " + address + " (tag: " + tag + "): miss - cached to row " + row);
                 }
                 else
                 {
+                    totalCPI++;
                     Console.WriteLine("Accessing: " + address + " (tag: " + tag + "): hit from row " + row);
                 }
             }
